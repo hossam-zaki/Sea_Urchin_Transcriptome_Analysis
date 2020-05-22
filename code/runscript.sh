eb2a2e91bfcf00916e3cdda5ca17e1da9baedcff
hossam-zaki <hossam_zaki@brown.edu>
2020-05-22 19:03:03 -0400

    updated runscript

--- code/runscript.sh
+++ code/runscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -n 4
 #SBATCH --mem=64G
-#SBATCH -t 1:00:00
+#SBATCH -t 2:00:00
 
-python3 miRNAsearch.py --seqs ../data/depleted_sequences_cds.txt --mirna ../data/miRNASeqs.txt
+python3 miRNAsearch.py --seqs ../data/motifSearchData/depleted_sequences.txt --mirna ../data/miRNAData/miRNASeqs.txt
